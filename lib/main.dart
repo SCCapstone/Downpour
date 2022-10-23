0131c7dd66808e12acc4f5c7621de39ca8b9579f
Kyle <kmeiler@email.sc.edu>
2022-10-23 16:22:23 -0400

    First Commit to Project
    I made it so that all button colors can be changed at once. I did this using a static variable. I also adjusted the spacing slightly.

--- lib/main.dart
+++ lib/main.dart
@@ -10,71 +10,87 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const Color background = Color.fromARGB(255, 16, 82, 182);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quiz app',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Take this short Quiz'),
-          backgroundColor: Colors.lightBlueAccent,
+          title: const Text('                   Take this short Quiz'),
+          backgroundColor: background,
         ),
-
         body: const Quiz(),
       ),
     );
   }
 }
+
 class Quiz extends StatelessWidget {
   const Quiz({super.key});
-
+  static const Color button = Color.fromARGB(255, 27, 105, 223);
   @override
   Widget build(BuildContext context) {
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
-      children:  <Widget>[
-        const Text(" Trivia Question: Which animal does not appear in the Chinese zodiac?",style: TextStyle(fontSize: 20,fontWeight:
-        FontWeight.bold),),
-        const SizedBox(height: 10),
-        ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.lightBlue,
+      children: <Widget>[
+        const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
+        const Text(
+          " Trivia Question: Which animal does not appear in the Chinese zodiac?",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          onPressed: () => Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => const WrongScreen())),child: const Text('Dragon'),
         ),
-        const SizedBox(height: 10),
-        ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.lightBlue,
+        const SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: button,
           ),
-          onPressed: () => Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => const WrongScreen())),child: const Text('Rabbit'),
+          onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const WrongScreen())),
+          child: const Text('Dragon'),
         ),
-        const SizedBox(height: 10),
-        ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.lightBlue,
+        const SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: button,
           ),
-          onPressed: () => Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => const WrongScreen())),child: const Text('Dog'),
+          onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const WrongScreen())),
+          child: const Text('Rabbit'),
         ),
-        const SizedBox(height: 10),
-        ElevatedButton(style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.lightBlue,
+        const SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: button,
           ),
-          onPressed: () => Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => const RightScreen())),child: const Text('Lion'),
+          onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const WrongScreen())),
+          child: const Text('Dog'),
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: button,
+          ),
+          onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const RightScreen())),
+          child: const Text('Lion'),
+        ),
+        const SizedBox(height: 20),
       ],
     );
   }
 }
+
 class WrongScreen extends StatefulWidget {
   const WrongScreen({Key? key}) : super(key: key);
 
   @override
   State<WrongScreen> createState() => _WrongScreenState();
 }
+
 class RightScreen extends StatefulWidget {
   const RightScreen({Key? key}) : super(key: key);
 
@@ -93,11 +109,16 @@ class _WrongScreenState extends State<WrongScreen> {
         backgroundColor: Colors.red,
         title: const Text('Answer'),
       ),
-      body: const Center(child: Text('Incorrect',style: TextStyle(fontSize: 20,fontWeight:
-      FontWeight.bold),textAlign: TextAlign.center,)),
+      body: const Center(
+          child: Text(
+        'Incorrect',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      )),
     );
   }
 }
+
 class _RightScreenState extends State<RightScreen> {
   TextEditingController textEditingController = TextEditingController();
 
@@ -109,8 +130,12 @@ class _RightScreenState extends State<RightScreen> {
         backgroundColor: Colors.green[400],
         title: const Text('The Answer'),
       ),
-      body: const Center(child: Text('Correct Answer!',style: TextStyle(fontSize: 30,fontWeight:
-      FontWeight.bold),textAlign: TextAlign.center,)),
+      body: const Center(
+          child: Text(
+        'Correct Answer!',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      )),
     );
   }
 }
