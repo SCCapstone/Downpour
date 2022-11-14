52d8ad0e176d3743513df2aa30c05bb4a77781d3
Nghia Nguyen <nghia@email.sc.edu>
2022-11-13 21:09:57 -0500

    Search screen scaffolding
    Just need to link it with database later and have some type of navigation to get to it.

--- lib/SearchScreen.dart
+++ lib/SearchScreen.dart
@@ -1,23 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:search_page/search_page.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+/// Simple class and list to link with database later
+class SearchItem implements Comparable<SearchItem> {
+  final String title, subtitle;
+  final num info; // for some other future functionality
 
-  static const String _title = "Coolest App Available Inc.";
-  static const Color appBarColor = Color.fromRGBO(135, 206, 235, 1.0);
-  static const Color background = Color.fromRGBO(255, 100, 100, 1.0);
+  const SearchItem(this.title, this.subtitle, this.info);
+
+  @override
+  int compareTo(SearchItem other) => title.compareTo(other.title);
+}
+
+const searchItems = [
+  SearchItem('Nghia', 'Nguyen', 111),
+  SearchItem('Cassidy', 'Meyers', 222),
+  SearchItem('William', 'Sutton', 333),
+  SearchItem('Tatiana', 'Washington', 444),
+  SearchItem('Kyle', 'Meiler', 555),
+  SearchItem('Pohnpei Basics', 'Nghia Nguyen', 2022),
+  SearchItem('How to get around the island', 'Will Sutton', 2019),
+];
+
+//void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: (background),
-            title: const Text(_title),
-            centerTitle: true),
-        body: const SearchScreen(),
-        backgroundColor: (background),
+      title: 'search_page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Page'),
+      ),
+      body: const Text("Press the search button to search"),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Search profiles, articles',
+        onPressed: () => showSearch(
+          context: context,
+          delegate: SearchPage(
+            onQueryUpdate: print,
+            items: searchItems,
+            searchLabel: 'Search',
+            suggestion: const Center(
+              child: Text('Filter items by title, subtitle or info'),
+            ),
+            failure: const Center(
+              child: Text('No items found :('),
+            ),
+            filter: (item) => [
+              item.title,
+              item.subtitle,
+              item.info.toString(),
+            ],
+            sort: (a, b) => a.compareTo(b),
+            builder: (item) => ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.subtitle),
+              trailing: Text('${item.info}'),
+            ),
+          ),
+        ),
+        child: const Icon(Icons.search),
       ),
     );
   }
