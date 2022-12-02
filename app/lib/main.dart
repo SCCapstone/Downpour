4bcb550017d48442606fcdc0fdea887df25b130e
Wsutton424 <wmsutton@email.sc.edu>
2022-12-02 18:26:20 -0500

    added user account creation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,11 +5,15 @@
 import 'package:flutter/material.dart';
 import 'screens/SignUpScreen.dart';
 //import 'package:pohnpeian_language_app/screens/pages/home.dart';
-
 //import 'screens/pages/result_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
