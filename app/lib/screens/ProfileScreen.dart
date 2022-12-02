430f67a32ec225a946355de904f8a6eb30c496b6
Nghia Nguyen <nghia@email.sc.edu>
2022-12-02 18:55:04 -0500

    Update for profile and quiz

--- app/lib/screens/ProfileScreen.dart
+++ app/lib/screens/ProfileScreen.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:pohnpeian_language_app/theme/style.dart' as customStyle;
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
 
   @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Profile Screen',
-            style: customStyle.appBarText,
-          ),
-          backgroundColor: customStyle.light,
-        ),
-        body: const Center(
-          child: Text('Fill in here'),
-        ),
-        backgroundColor: customStyle.primary,
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("Profile")
+            //title: Text(widget.title, style: custom_style.appBarText),
+            //backgroundColor: custom_style.light),
+            ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                height: 60,
+                width: 60,
+                child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg"))),
+            Text("NAME"),
+            Container(
+                margin: EdgeInsets.all(30),
+                child: Text(
+                    "BIO: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
+            TextButton(onPressed: () {}, child: Text("Delete Account")),
+          ],
+        ));
   }
 }
