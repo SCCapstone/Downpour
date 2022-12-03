5a82da045e5aad43ee27a4b5189d4234df3a81d8
Wsutton424 <wmsutton@email.sc.edu>
2022-12-02 19:23:45 -0500

    -delete User Account

--- app/lib/screens/ProfileScreen.dart
+++ app/lib/screens/ProfileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -29,7 +30,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 margin: EdgeInsets.all(30),
                 child: Text(
                     "BIO: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
-            TextButton(onPressed: () {}, child: Text("Delete Account")),
+            TextButton(onPressed: () {
+              final docUser = FirebaseFirestore.instance
+			          .collection('Users')
+			          .doc('userBase');
+			
+			       docUser.set({
+				       'name': '',
+			});
+
+            }, child: Text("Delete Account")),
           ],
         ));
   }
