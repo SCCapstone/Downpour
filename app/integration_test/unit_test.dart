18ef58cb99905da7ce6b6ec273072d13dc0ed1fe
Kyle <kmeiler@email.sc.edu>
2023-01-29 07:41:21 -0500

    Update unit_test.dart

--- app/integration_test/unit_test.dart
+++ app/integration_test/unit_test.dart
@@ -4,9 +4,14 @@ import 'package:test/test.dart';
 import 'package:pohnpeian_language_app/main.dart' as app;
 
 void main() {
-  test ('empty email returns error', ()
+  test ('empty password returns error', ()
   {
     var result = PasswordFieldValidator.validate('');
     expect(result, 'Password can\'t be empty');
+  });
+  test ('non-empty password returns null', ()
+  {
+    var result = PasswordFieldValidator.validate('');
+    expect(result, null);
   });
   }
